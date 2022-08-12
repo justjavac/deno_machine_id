f13a83bf7b39f81fb3d10f1cd41e294e9d20531f
迷渡 <justjavac@gmail.com>
2022-08-12 17:54:53 +0800

    upgrade std@0.152.0

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import { assertNotEquals } from "https://deno.land/std@0.150.0/testing/asserts.ts";
+import { assertNotEquals } from "https://deno.land/std@0.152.0/testing/asserts.ts";
 import machineId from "./mod.ts";
 
 const { test } = Deno;
